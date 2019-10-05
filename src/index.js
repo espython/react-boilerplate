b0ccad7a38ee74a1a35d599fe5497fcc3e60768d
espython <espython85@gmail.com>
2019-10-05 02:35:06 +0200

    add eslint-import support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Action Increment
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  };
+};
+
+// Action Increment
+const decrement = () => {
+  return {
+    type: 'INCREMENT'
+  };
+};
+
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
